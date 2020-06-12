595b6add57fc51f9c59ed1519139030405878b71
Pushp Singh <pushp.singh@SW-LP02545.local>
2020-06-12 10:50:30 +0530

    added styles to card

--- src/scripts/card.js
+++ src/scripts/card.js
@@ -18,6 +18,7 @@ const saveCard = function (event) {
     }
     userCardList.push(userDetails)
     window.localStorage.setItem('cardDetails', JSON.stringify(userCardList))
+    window.alert("Card Saved Successfully! \n Please refresh")
 
 };
 
@@ -32,10 +33,13 @@ if (window.localStorage.getItem('cardDetails') !== null) {
             event.preventDefault();
 
             const createform = document.createElement('form');
+            createform.setAttribute("style", "position:relative;display:contents;text-align:center;justify-content:center;width:100%");
+            // createform.setAttribute("style", "display:flex;text-align:center;justify-content:center;width:100%");
             document.body.appendChild(createform);
 
             let numberLabel = document.createElement('label');
             numberLabel.innerHTML = "Card Number";
+            numberLabel.setAttribute("style", "color:black;font-size:19px;");
             createform.appendChild(numberLabel);
 
             var linebreak = document.createElement('br');
@@ -45,18 +49,16 @@ if (window.localStorage.getItem('cardDetails') !== null) {
             cardNumber.setAttribute("type", 'text');
             cardNumber.setAttribute('name', data.cardNumber);
             cardNumber.setAttribute("value", data.cardNumber.replace(/[^\dA-Z]/g, '').replace(/(.{4})/g, '$1 ').trim());
+            cardNumber.setAttribute("style", "width:15%;padding:10px");
             createform.appendChild(cardNumber);
 
             var linebreak = document.createElement('br');
             createform.appendChild(linebreak);
 
             let validityLabel = document.createElement('label');
-            validityLabel.innerHTML = "Validity"
+            validityLabel.innerHTML = "Validity";
+            validityLabel.setAttribute("style", "color:black;font-size:19px;");
             createform.appendChild(validityLabel)
-
-            let cvvLabel = document.createElement('label');
-            cvvLabel.innerHTML = "Card CVV"
-            createform.appendChild(cvvLabel)
 
             var linebreak = document.createElement('br');
             createform.appendChild(linebreak);
@@ -65,23 +67,34 @@ if (window.localStorage.getItem('cardDetails') !== null) {
             validity.setAttribute("type", 'text');
             validity.setAttribute("name", data.validity);
             validity.setAttribute("value", data.validity.replace(/\s/g, '').replace(/(\d{2})/g, '$1 ').trim());
+            validity.setAttribute("style", "width:15%;padding:10px");
             createform.appendChild(validity)
+
+            var linebreak = document.createElement('br');
+            createform.appendChild(linebreak);
+
+            let cvvLabel = document.createElement('label');
+            cvvLabel.innerHTML = "Card CVV";
+            cvvLabel.setAttribute("style", "color:black;font-size:19px;");
+            createform.appendChild(cvvLabel);
+
+            var linebreak = document.createElement('br');
+            createform.appendChild(linebreak);
 
             const cvv = document.createElement('input'); // Card CVV Field
             cvv.setAttribute("type", 'password');
             cvv.setAttribute("name", data.cvv);
             cvv.setAttribute("value", data.cvv);
+            cvv.setAttribute("style", "width:15%;padding:10px");
             createform.appendChild(cvv)
 
 
             var linebreak = document.createElement('br');
             createform.appendChild(linebreak);
 
-            var linebreak = document.createElement('br');
-            createform.appendChild(linebreak);
-
             let userLabel = document.createElement('label');
             userLabel.innerHTML = "Card Holder Name";
+            userLabel.setAttribute("style", "color:black;font-size:19px;");
             createform.appendChild(userLabel);
 
             var linebreak = document.createElement('br');
@@ -91,6 +104,7 @@ if (window.localStorage.getItem('cardDetails') !== null) {
             name.setAttribute("type", "text");
             name.setAttribute('name', data.cardHolderName);
             name.setAttribute("value", data.cardHolderName);
+            name.setAttribute("style", "width:15%;padding:10px");
             createform.appendChild(name);
 
             var linebreak = document.createElement('br');
@@ -101,7 +115,7 @@ if (window.localStorage.getItem('cardDetails') !== null) {
             button.setAttribute("name", "Delete");
             button.setAttribute("value", "Delete Card");
             button.setAttribute("onclick", `deleteCard(${userCardList.indexOf(data)})`);
-            button.setAttribute("style", "color:red;font-size:23px");
+            button.setAttribute("style", "color:red;font-size:13px;width:12%;padding:5px;");
             createform.appendChild(button);
 
             var linebreak = document.createElement('br');
